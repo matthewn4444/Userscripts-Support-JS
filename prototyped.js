3713db6b0efc15972f8c255d9f8ec32ed297dfd2
Matthew Ng <matthewn4444@gmail.com>
2013-12-06 22:16:52 -0500

    New version with more functions

--- prototyped.js
+++ prototyped.js
@@ -1,69 +1,148 @@
-/*==================================*\
-               STRINGS
-\*==================================*/
-String.prototype.contains = function(s) {
-    return this.indexOf(s)!=0;
+function isArray(o) {
+    return Object.prototype.toString.call( o ) === '[object Array]';
 }
-String.prototype.empty = function() {
-    return this=="";
+function isObject(o) {
+    return Object.prototype.toString.call( o ) === '[object Object]';
 }
-String.prototype.capitalize = function(){
-    return this.replace( /(^|\s)([a-z])/g , function(m,p1,p2){ return p1+p2.toUpperCase(); } );
-};
-String.prototype.escapeHTML = function(){
-    return String(this).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
-};
-String.prototype.underscorize = function() {
-   return this.replace(/\s|-/g, "_");
+function isString(o) {
+    return Object.prototype.toString.call( o ) === '[object String]';
 }
-String.prototype.startsWith = function(str) {
-   if (str == null || str.length == 0) {return false;}
-   return this.substring(0, str.length) == str;
+function isFunction(o) {
+    return o && Object.prototype.toString.call( o ) === '[object Function]';
 }
-String.prototype.setCharAt = function(index, character) {
-   if (index < 0) {index = this.length + index;}
-   return this.substr(0, index) + character + this.substr(index+character.length);
-}
-String.prototype.removeWords = function(listOfWords){
-   if (Object.prototype.toString.call(listOfWords) === '[object Array]' 
-        || listOfWords.empty()) {
-      return this;
-   }
-   var regexStr = "(^|\\W)("+listOfWords[0];
-   for(var i=1;i<listOfWords.length;i++) {
-      regexStr += "|" + listOfWords[i];
-   }
-   regexStr += ")(\\W|$)";
-   return this.replace(new RegExp(regexStr,"gi"), "");
-}
-String.prototype.getTextBetween=function(front, end){
-   if (this==null){return null;}
-   var startIndex=0;
-   var endIndex=this.length-1;
-   if (front!=null){startIndex=this.indexOf(front)+front.length;}
-   if (end!=null){endIndex=this.lastIndexOf(end);}
-   return this.substring(startIndex,endIndex);
-};
 
 /*==================================*\
-               ARRAYS
+              Prototype
 \*==================================*/
-Array.prototype.foreach = function(fn) {
-    if(fn) for (var i=0; i<this.length;i++) fn(this[i],i); else throw new Error("Array.prototype.foreach: no callback");
+(function(object){
+function implement(Class, implementationObj) {
+    for (var i in implementationObj) {
+        if (implementationObj.hasOwnProperty(i)) {
+            Class.prototype[i] = implementationObj[i];
+        }
+    }
 }
-Array.prototype.reverseForeach = function(fn) {
-    if(fn) for (var i=this.length-1;i>=0;--i) fn(this[i],i); else throw new Error("Array.prototype.reverseForeach: no callback");
-}
-Array.prototype.empty = function() {
-    return this.length===0;
-}
-Array.prototype.contains = function(val) {
-    return this.indexOf(val)>=0;
-}
-Array.prototype.clone = function(){
-   return this.concat([]);
-}
-Array.prototype.last = function(index){
-   if (this.empty()) {return null;}
-   return this[this.length-1];
-}
+
+// String
+implement(String, {
+    between: function(begin, end, index) {
+        index = index || 0;
+        var sIndex = this.indexOf(begin, index);
+        if (sIndex == -1) return null;
+        sIndex += begin.length;
+        var eIndex = this.indexOf(end, sIndex);
+        if (eIndex == -1) return null;
+        return this.substring(sIndex, eIndex);
+    },
+    capitalize: function() {
+        return this.charAt(0).toUpperCase() + this.slice(1);
+    },
+    contains: function(s) {
+        return this.indexOf(s)!=0;
+    },
+    endsWith: function(s) {
+        if (this.length < s.length) return false;
+        return this.substr(this.length - s.length) === s;
+    },
+    escapeHTML: function() {
+        return String(this).replace(/&/g, '&amp;').replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+    },
+    isEmail: function() {
+        var r = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return r.test(this);
+    },
+    isEmpty: function() {
+        return this == "";
+    },
+    isUrl: function() {
+        var http = "http://";
+        return this.substring(0, http.length) === http;
+    },
+    removeExtension: function() {
+        if (this.length > 4 && this[this.length - 4] == '.') {
+            return this.substring(0, this.lastIndexOf('.'));
+        }
+        return this + "";
+    },
+    removeWords: function(listOfWords) {
+        if (isArray(listOfWords) || listOfWords.empty()) {
+            return this;
+        }
+        var regexStr = "(^|\\W)("+listOfWords[0];
+        for(var i=1;i<listOfWords.length;i++) {
+            regexStr += "|" + listOfWords[i];
+        }
+        regexStr += ")(\\W|$)";
+        return this.replace(new RegExp(regexStr,"gi"), "");
+    },
+    setCharAt: function(index, character) {
+        if (index < 0) {index = this.length + index;}
+        return this.substr(0, index) + character + this.substr(index+character.length);
+    },
+    startsWith: function(s) {
+        if (!s || s.length == 0 || s.length > this.length) {return false;}
+        return this.substring(0, s.length) == s;
+    },
+    underscorize: function() {  
+        return this.replace(/\s|-/g, "_");
+    },
+    upTo: function(string, index, dontInclude) {
+        var i = this.indexOf(string, index);
+        index = index || 0;
+        if (!i) {return null;}
+        return this.substring(index, i) + (!dontInclude ? string : "");
+    },
+});
+
+// Array
+implement(Array, {
+    contains: function(v) {
+        return this.indexOf(v)>=0;
+    },
+    hasOneOf: function(arr) {
+        if (!arr || isArray(arr)) return;
+        for (var i = 0; i < arr.length; i++) {
+            if (this.indexOf(i) !== -1) {
+                return true;
+            }
+        }
+        return false;
+    },
+    clone: function() {
+        return this.concat([]);
+    },
+    each: function(fn) {
+        if(!fn || !isFunction(fn)) return;
+        for (var i=0; i<this.length;i++) 
+            fn(i, this[i]); 
+    },
+    empty: function() {
+        return this.length === 0;
+    },
+    last: function() {
+        if (!this.length) {return null;}
+        return this[this.length-1];
+    },
+    rEach: function(fn) {
+        if(!fn || !isFunction(fn)) return;
+        for (var i=this.length-1;i>=0;--i) 
+            fn(i, this[i]); 
+    },
+});
+
+// Object
+implement(Object, {
+    each: function(fn) {
+        if (!fn && !isFunction(fn))
+            for (var i in this)
+                if (this.hasOwnProperty(i))
+                    fn(i, this[i]);
+    },
+});
+})();
+
+
+
+
+
