75f5a5dbd72ea3cb20c4ef5859a940d0bbfe4be0
Matthew Ng <matthewn4444@gmail.com>
2013-12-11 18:29:46 -0500

    Added utils file which includes a get function

---
+++ utils.js
@@ -0,0 +1,33 @@
+if (typeof get !== "undefined") {
+window.get = function(url, callback) {
+    callback = callback || function(){};
+    function onload(response) {
+        if (response && response.status === 200) {
+            callback(response.responseText);
+        } else {	
+            callback(null);
+        }
+    }
+    if (typeof GM_xmlhttpRequest !== "undefined") {
+        // Cross domain
+        GM_xmlhttpRequest({
+            method:"GET",
+            url: url,
+            overrideMimeType: 'text/plain; charset=x-user-defined',
+            onload: onload
+        });
+    } else {
+        var req = new XMLHttpRequest();
+        req.open("GET", url, true);
+        req.overrideMimeType('text/plain; charset=x-user-defined');
+        req.onload = function(e) {
+            if (e) {
+                onload(e.target);
+            } else {
+                callback();
+            }
+        }
+        req.send();
+    }
+}
+}
